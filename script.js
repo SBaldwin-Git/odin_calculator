4ce13e119f3ebf1e4f3d521b497e0cb297cf8032
Sam Baldwin <sambaldwin92@gmail.com>
2021-12-29 15:42:05 +0000

    operators and numbers now seperate in array

--- script.js
+++ script.js
@@ -39,17 +39,34 @@ divide = (num1, num2) => num1 + num2;
 function inputNumber() {
     let buttonContent = this.textContent;
 
+    //Stops first entry being operator
     if (calcString.length < 1 && isOperator(buttonContent) == true) {
         return;
     }
 
     //If button pressed is an Operator AND so is the last in the Array
     if (isOperator(buttonContent) == true && isOperator(calcString[calcString.length - 1]) == true) {
+        //Changes operator via pop
         calcString.pop();
         calcString.push(buttonContent);
-    } else {
+    } else if (isOperator(buttonContent) == true) {
+        //Add operator to new array item
         calcString.push(buttonContent);
+    } else {
+        if (calcString.length == 0) {
+            calcString[0] = buttonContent;
+        } else {
+            //Checks if current array entry is a operator or number
+            if (isOperator(calcString[calcString.length - 1]) == true) {
+                //If it is an operator, create new array item
+                calcString.push(buttonContent);
+            } else {
+                //if not then to existing array item
+                calcString[calcString.length - 1] = calcString[calcString.length - 1] + buttonContent;
+            }
+        }
     }
+    //Update display header
     calcH1.textContent = calcString.join('');
 }
 
@@ -59,6 +76,10 @@ function isOperator(clickedButton) {
     } else {
         return false;
     }
+}
+
+function isNumber() {
+
 }
 
 clearButton.addEventListener('click', function() {
