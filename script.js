a98afce35a226bd2decb6486288814e411a7a165
Sam Baldwin <sambaldwin92@gmail.com>
2021-12-29 12:11:07 +0000

    buttons now update display text with array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let calcH1 = document.querySelector('#calcText');
 let calcString = [];
-const equalsButton = document.querySelector('#btn='),
+let clearButton = document.querySelector('#btnClear');
+let equalsButton = document.querySelector('#btnEquals');
 
 const numButtons = [
     document.querySelector('#btn0'),
@@ -15,18 +16,33 @@ const numButtons = [
     document.querySelector('#btn9')
 ];
 
+for(let i =0;i < numButtons.length;i++){
+    numButtons[i].addEventListener('click', inputNumber);
+}
+
 const operatorButtons = [
-    document.querySelector('#btn+'),
-    document.querySelector('#btn-'),
-    document.querySelector('#btn*'),
-    document.querySelector('#btn/')
+    document.querySelector('#btnPlus'),
+    document.querySelector('#btnMinus'),
+    document.querySelector('#btnMultiply'),
+    document.querySelector('#btnDivide')
 ];
+
+for(let i =0;i < operatorButtons.length;i++){
+    operatorButtons[i].addEventListener('click', inputNumber);
+}
 
 add = (num1, num2) => num1 + num2;
 minus = (num1, num2) => num1 - num2;
 multiply = (num1, num2) => num1 * num2;
 divide = (num1, num2) => num1 + num2;
 
-function inputNumber(e){
-    calcString += this.innerHTML;
+function inputNumber(){
+    calcString.push(this.textContent);
+    calcH1.textContent = calcString.join('');
 }
+
+clearButton.addEventListener('click', function(){
+    calcString = [];
+    // calcH1.textContent = '0';
+    calcH1.textContent = '0';
+})
